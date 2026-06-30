cad00ad30ef64b5febd0c5089e960fa2bf867362
shreyanspeaking-arch <shreyanspeaking@gmail.com>
2026-06-30 23:05:05 +0530

    Refactor time handling and DataFrame volume calculations
    Updated time handling in lists and improved DataFrame calculations.

--- One_Day_Network_Study_Plan.py
+++ One_Day_Network_Study_Plan.py
@@ -14,7 +14,6 @@ l=[t]
 for i in range (n):
     t+=f
     l.append(t)
-    
 for i in range(n):
     l2=[np.nan for j in range(3)]
     l2[0]=l[0].time()
@@ -52,8 +51,8 @@ for i in range (n):
 for i in range(n):
     l2=[np.nan for j in range(4)]
     l2[0]=input('Enter Location Name')
-    l2[1]=l[0]
-    l2[2]=l[1]
+    l2[1]=l[0].time()
+    l2[2]=l[1].time()
     l.pop(0)
     print('Enter vehicle count between',l2[1],'and',l2[2],'at location',l2[0])
     l2[3]=int(input())
@@ -61,8 +60,8 @@ for i in range(n):
 d2=pd.DataFrame(l1,columns=c)
 d2=d2.set_index(['Start Time','End Time'])
 string2='Estimated '+str(n1)+' hour volume'
-d2[string2]=d2['Count']/d1[string1]
-d2['Estimated Peak Hour Volume']=d2[string2]*np.max(d1[string1])
+d2[string2]=d2['Count']/d1[string1].iloc[:len(d2)]
+d2['Estimated Peak Hour Volume']=(d2[string2]*np.max(d1[string1])).astype(int)
 print(d2)
 with pd.ExcelWriter('One_Day_Network_Study_Plan.xlsx') as w:
     d1.to_excel(w,sheet_name='Control_Count_Data',index=False)
