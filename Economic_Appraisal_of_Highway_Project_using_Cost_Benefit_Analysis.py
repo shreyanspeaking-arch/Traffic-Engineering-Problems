2998ef88ec0299e020c913f5127016a1763ba1dd
shreyanspeaking-arch <shreyanspeaking@gmail.com>
2026-07-20 12:32:01 +0530

    Enhance cost analysis with user-defined functions
    Updated the calculation of average vehicle operating costs to allow user-defined functions and improved handling of discounted benefits and costs.

--- Economic_Appraisal_of_Highway_Project_using_Cost_Benefit_Analysis.py
+++ Economic_Appraisal_of_Highway_Project_using_Cost_Benefit_Analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import sympy as sp
+print('Make sure all the monetary values are of the same currency')
 t=int(input('Enter the economic life of Highway Proposal'))
 l=[np.nan for i in range(t)]
 t1=int(input('Enter no of years taken for initial construction of highway before inauguration'))
@@ -11,13 +13,13 @@ avts=int(input('Enter average vehicle time savings per hour'))
 avs1=float(input('Average vehicle speed on existing road in km/h'))
 avs2=float(input('Average vehicle speed on upgraded road in km/h'))
 dis=int(input('Enter discount rate in %'))
-#The equation for average vehicle operation cost savings per km
-#has to be entered in the program as a function of average vehicle speed 
-print('The equation for average vehicle operation cost savings per km per vehicle per year \n','has to be entered in the program as a function of average vehicle speed. \n','Change it if needed.')
-def avoc(x):
-    return (2+(35/x)+(0.00005*(x**2)))/100
-avoc1=avoc(avs1)
-avoc2=avoc(avs2)
+V=sp.symbols('V')
+print('''Enter the function for Average Vehicle Operating Costs as single variable function of Average Vehicle speeds V. 
+Don''t include LHS and = like f(x)= and the output unit should be in cost/km''')
+f1=input()
+f2=sp.sympify(f1)
+avoc1=f2.subs({V:avs1})
+avoc2=f2.subs({V:avs2})
 for i in range(t):
     k=[np.nan for i in range(4)]
     k[0]=i+1
@@ -41,5 +43,12 @@ d['Discounted Benefits']=d['Total User Benefits']/((1+(dis/100))**d['Year'])
 d['Construction and Maintenance Cost']=d['Construction Cost'].fillna(0)+d['Operating Cost'].fillna(0)
 d['Discounted Costs']=d['Construction and Maintenance Cost']/((1+(dis/100))**d['Year'])
 d=d.set_index('Year')
-print(d)
-d.to_excel('output.xlsx', sheet_name='Sheet1', index=False)
+d['Discounted Benefits']=d['Discounted Benefits'].fillna(0)
+d['Discounted Costs']=d['Discounted Costs'].fillna(0)
+del sp
+Net_Present_Value=float(np.nansum(d['Discounted Benefits'])-np.nansum(d['Discounted Costs']))
+if Net_Present_Value>0:
+    print('This project is acceptable with a Net Present Value of ',Net_Present_Value)
+else:
+    print('This project is not suitable')
+d.to_excel('output_Program1.xlsx', sheet_name='Sheet1', index=False)
