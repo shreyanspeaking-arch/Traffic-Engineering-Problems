1a8c9144baa2589eb459875e0f0dc489fcbd643a
shreyanspeaking-arch <shreyanspeaking@gmail.com>
2026-08-08 19:13:39 +0530

    Enhance coverage count adjustment with Excel export
    Add functionality to reset index and export adjusted coverage counts to Excel.

--- Multiday_Network_Study_Plan.py
+++ Multiday_Network_Study_Plan.py
@@ -10,9 +10,10 @@ t1=[np.nan for i in range(n)]
 t2=[np.nan for i in range(n)]
 l1=[]
 for i in range(n):
+    print('Dataset ',i+1)
     l=[np.nan for i in range(2)]
     l[0]=pd.to_datetime(input('Enter date in the form of DD/MM/YYYY'),dayfirst=True)
-    print(string)
+    print('Enter ',string,' on ',l[0].date())
     l[1]=int(input())
     l1+=[l]
 d1=pd.DataFrame(l1,columns=c1)
@@ -20,8 +21,9 @@ print('Enter Coverage Count Data')
 l2=[]
 for i in range(n):
     l=[np.nan for i in range(3)]
+    print('Dataset ',i+1)
     l[0]=input('Enter coverage location')
-    print(string)
+    print('Enter ',string,' on ',d1.loc[list(d1.index)[i],'Day'].date(),' at ',l[0])
     l[2]=int(input())
     l2+=[l]
 d2=pd.DataFrame(l2,columns=c2)
@@ -32,8 +34,12 @@ print('Data for a/an',n,'day study')
 print(d1)
 print(d2)
 d1['Adjustment Factor']=(np.nanmean(d1[string]))/d1[string]
-d2['Adjusted'+string]=d2[string]*d1['Adjustment Factor']
+d2['Adjusted '+string]=d2[string]*d1['Adjustment Factor']
+d2['Adjusted '+string]=d2['Adjusted '+string].astype('int64')
 print('Computation of Adjustment Factors')
 print(d1)
 print('Adjustment of Coverage Counts')
 print(d2)
+d2=d2.reset_index()
+f=input('Enter output filename for Adjustment of Coverage Counts Table. Exclude .xlsx')
+d2.to_excel(f+'.xlsx',index=False)
