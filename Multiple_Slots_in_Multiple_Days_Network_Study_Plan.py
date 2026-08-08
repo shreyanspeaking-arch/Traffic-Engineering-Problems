4b3f754bb7b62b312ac556eb1e937bb7481c6db0
shreyanspeaking-arch <shreyanspeaking@gmail.com>
2026-08-08 20:02:13 +0530

    Refactor input handling and calculations in study plan

--- Multiple_Slots_in_Multiple_Days_Network_Study_Plan.py
+++ Multiple_Slots_in_Multiple_Days_Network_Study_Plan.py
@@ -32,14 +32,14 @@ for i in range(1,d*2,2):
     df.iloc[i]=((df.iloc[i-1])/np.sum(df.iloc[i-1]))*100
 print('Control data and calibration of hourly variation pattern')
 print(df)
-df2=df.iloc[::s].copy()
+df2=df.iloc[::2].copy()
 string2=str(h)+'-Hour Control Count Location A (vehs)'
 df2[string2]=np.nansum(df2,axis=1)
 df2['Adjustment Factor']=np.nanmean(df2[string2])*((df2[string2])**(-1))
 print('Calibration of daily variation factors')
 print(df2)
 index=list(index)
-index=index[::s]
+index=index[::2]
 index2=[]
 for i in list(index):
     t=list(i)
