294681b468b6f59ac838ad938854a1d175997ca9
Shreyan Pal <shreyanpal.2026.2006@gmail.com>
2026-08-09 22:45:55 +0530

    Fix input prompts and output formatting

--- Parabolic_Curves_Computation_For_Vertical_Alignment_on_Highways.py
+++ Parabolic_Curves_Computation_For_Vertical_Alignment_on_Highways.py
@@ -1,7 +1,7 @@
 import sys
 print('All values in this code are in m')
 t1=tuple(eval(input('Enter coordinates of Tangent Point T1 in the form (x,y)')))
-t2=tuple(eval(input('Enter coordinates of Tangent Point T1 in the form (x,y)')))
+t2=tuple(eval(input('Enter coordinates of Tangent Point T2 in the form (x,y)')))
 if len(t1)!=2 or len(t2)!=2:
     print('Invalid Input')
     sys.exit()
@@ -12,14 +12,15 @@ p/=100
 print('Enter slope with sign at '+str(t2)+' in %')
 q=float(input())
 q/=100
+print('''Choose what you want to get:
+         1. K value of highway (Horizontal distance required for 1% change in grade
+         2. Coordinates of a point on the curve (requires x-coordinate)
+         3. Vertical offset(e) at the point of intersection of two tangents
+         4. Vertical offset at a point of the curve (requires x-coordinate)
+         5. Horizontal and Vertical Offsets at the highest point on the curve
+         ''')
 c=''
 while c.upper()!='N':
-    print('''Choose what you want to get:
-1. K value of highway (Horizontal distance required for 1% change in grade
-2. Coordinates of a point on the curve (requires x-coordinate)
-3. Vertical offset(e) at the point of intersection of two tangents
-4. Vertical offset at a point of the curve (requires x-coordinate)
-5. Horizontal and Vertical Offsets at the highest point on the curve''')
     o=int(input('Enter the number for the option you want to select'))
     if o==1:
         K=L/((p-q)*100)
@@ -27,7 +28,7 @@ while c.upper()!='N':
     elif o==2:
         x=float(input('Enter x coordinate'))
         Y=(((q-p)/L)*((x**2)/2))+(p*x)
-        print('The coordinates of point are x,Y taking T1 as origin are '+str(tuple(x,Y)))
+        print('The coordinates of point are x,Y taking T1 as origin are '+str(tuple([x,Y])))
     elif o==3:
         e=(p-q)*(L/8)
         print('The vertical offset at the point of two tangents at point of intersection is ',e)
@@ -35,7 +36,7 @@ while c.upper()!='N':
         x=float(input('Enter x coordinate'))
         y=((p-q)/(2*L))*(x**2)
         Y=(((q-p)/L)*((x**2)/2))+(p*x)
-        print('The vertical offset at '+str(tuple(x,Y))+' is ',y)
+        print('The vertical offset at '+str(tuple([x,Y]))+' is ',y)
     elif o==5:
         x=(L*p)/(p-q)
         y=(L*(p**2))/(2*(p-q))
